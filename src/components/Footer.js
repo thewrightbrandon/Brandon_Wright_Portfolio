d73e0a160f4367450eeb4ebdaece13bb77a7a662
Brandon Wright <thewrightbrandon@gmail.com>
2021-06-10 19:22:19 -0400

    Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,18 @@ class Footer extends React.Component {
   render = () => {
     return (
       <footer>
-        <h5>Footer</h5>
+        <a className="footerLink" href="https://www.linkedin.com/in/brandon-m-wright/"
+        rel="noreferrer" target="_blank">
+          <img className="footerIcon" src='images/linkedin-256.png' alt="LinkedIn Logo"/>
+        </a>
+        <a className="footerLink" href="mailto:thewrightbrandon@gmail.com"
+        rel="noreferrer" target="_blank">
+          <img className="footerIcon" src='images/email-256.png' alt="Envelope Icon"/>
+        </a>
+        <a className="footerLink" href="https://github.com/thewrightbrandon"
+        rel="noreferrer" target="_blank">
+          <img className="footerIcon" src='images/github-11-256.png' alt="Github Logo"/>
+        </a>
       </footer>
     )
   }
